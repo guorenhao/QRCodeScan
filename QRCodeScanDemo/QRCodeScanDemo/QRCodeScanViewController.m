6b076abdda7ba1dace8b9c37574d7ab3b405921a
Abner_G <grh_1990@126.com>
2016-10-27 22:56:59 +0800

    二维码扫描

--- QRCodeScanDemo/QRCodeScanDemo/QRCodeScanViewController.m
+++ QRCodeScanDemo/QRCodeScanDemo/QRCodeScanViewController.m
@@ -3,7 +3,7 @@
 //  MCDY
 //
 //  Created by 郭人豪 on 16/8/22.
-//  Copyright © 2016年 瞄财网络科技（北京）有限公司. All rights reserved.
+//  Copyright © 2016年 Abner_G. All rights reserved.
 //
 
 #import "QRCodeScanViewController.h"
